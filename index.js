9007e8c47316def89ecdd37d328cc5cbe422a78a
mdimranictiu <imranictiu11@gmail.com>
2024-12-21 16:17:37 +0600

    neww

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const app=express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const app = express();
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const Secret_key = process.env.SECRET;
 
-const port=process.env.PORT || 3000;
-app.use(cors())
-app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send('Job portal')
-})
+const port = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Job portal");
+});
 
+const logger = (req, res, next) => {
+  console.log("inside the logger");
+  next();
+};
+const verifyToken = (req, res, next) => {
+  console.log("token here", req.cookies);
+  const token = req?.cookies?.token;
 
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Author" });
+  }
+  jwt.verify(token, Secret_key, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    next();
+
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nu3ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,24 +48,56 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const database = client.db("JobPortal");
+    const jobCollection = database.collection("Jobs");
+
+    // apis
+    app.get("/jobs", logger, verifyToken, async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+      console.log("cuk cuk ", req.cookies);
+    });
+
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Auth Apis
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, Secret_key, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    // Send a ping to confirm a successful connection
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.listen(port,()=>{
-    console.log(`job is waiting at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`job is waiting at http://localhost:${port}`);
+});
